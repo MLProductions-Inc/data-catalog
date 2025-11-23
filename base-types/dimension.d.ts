cec48f4471af56555a466db603ec2b508e639fdd
Monica D. <moniqx4@gmail.com>
2025-11-23 07:49:27 -0700

    Add geometric and dimension type definitions

---
+++ base-types/dimension.d.ts
@@ -0,0 +1,22 @@
+// ============================================================================
+// GEOMETRIC & DIMENSION TYPES
+// ============================================================================
+
+declare interface WidthHeightType { width: number; height: number }
+
+declare interface XY_Type { x: number; y: number }
+
+// use for padding, margin and box spacing
+declare interface DirectionType {
+  top: number
+  right: number
+  bottom: number
+  left: number
+}
+
+declare interface BasePositionType {
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+}
