d80100b2ed67d1fe18bf09ac705ee7248e0cdee2
Monica D. <moniqx4@gmail.com>
2025-11-23 07:44:17 -0700

    Add type definitions for UI components

---
+++ simple-types/component.d.ts
@@ -0,0 +1,11 @@
+// ============================================================================
+// UI COMPONENT TYPES
+// ============================================================================
+
+declare type TButtonVariant = "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | null | undefined
+
+declare type TButtonSize = 'sm' | 'default' | 'lg' | 'icon'
+
+declare type THeaderType = 'h5' | 'h4' | 'h3' | 'h2' | 'h1'
+
+declare type TSelection = 'single' | 'multiple'
