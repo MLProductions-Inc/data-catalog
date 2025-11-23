9033477f2eb9e13363191aca3cb5b1476768a9f3
Monica D. <moniqx4@gmail.com>
2025-11-23 07:53:50 -0700

    Rename THeaderType to THeader in component.d.ts

--- simple-types/component.d.ts
+++ simple-types/component.d.ts
@@ -6,6 +6,6 @@ declare type TButtonVariant = "link" | "default" | "destructive" | "outline" | "
 
 declare type TButtonSize = 'sm' | 'default' | 'lg' | 'icon'
 
-declare type THeaderType = 'h5' | 'h4' | 'h3' | 'h2' | 'h1'
+declare type THeader = 'h5' | 'h4' | 'h3' | 'h2' | 'h1'
 
 declare type TSelection = 'single' | 'multiple'
