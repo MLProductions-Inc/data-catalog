bf34e7cc98cb2f5da3338730a27eeaa55011f6c0
Monica D. <moniqx4@gmail.com>
2025-11-29 06:16:32 -0700

    Add enums for platforms, payment methods, and roles

---
+++ enums/enums.ts
@@ -0,0 +1,27 @@
+export enum SM_PLATFORM {
+  FACEBOOK = 'Facebook',
+  LINKEDIN = 'LinkedIn',
+  INSTAGRAM = 'Instagram',
+  TWITTER = 'Twitter/X',
+  YOUTUBE = 'YouTube',
+  TELEGRAM = 'Telegram',
+}
+
+export enum PAYMENTMETHOD {
+  VISA,
+  MASTERCARD,
+  ZELLE,
+  CASHAPP,
+  DISCOVER,
+  AMEX,
+  PAYPAL,
+  STRIPE,
+}
+
+export enum ROLE {
+  ADMIN,
+  MEMBER,
+  CUSTOMER,
+  CLIENT,
+  MANAGER,
+}
