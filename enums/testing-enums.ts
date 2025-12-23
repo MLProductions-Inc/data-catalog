b62929609df8c83e5315bb43816a533863b202fd
Monica D. <moniqx4@gmail.com>
2025-12-23 05:59:03 -0700

    Add enums for priority, severity, and risk level
    Defined enums for PRIORITY, SEVERITY, and RISK_LEVEL.

---
+++ enums/testing-enums.ts
@@ -0,0 +1,21 @@
+export enum PRIORITY {
+  LOW = 'p3',
+  MEDIUM = 'p2',
+  HIGH = 'p1',
+  CRITICAL = 'p0',
+}
+
+export enum SEVERITY {
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high',
+  CRITICAL = 'critical',
+}
+
+export enum RISK_LEVEL {
+  R0 = 'R0',
+  R1 = 'R1',
+  R2 = 'R2',
+  R3 = 'R3',
+  R4 = 'R4',
+}
